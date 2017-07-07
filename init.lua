9aab4b3c061ae2ab976eb24a7c6e5f70d99d80ff
ezhh <owlecho@live.com>
2017-07-07 22:42:07 +0100

    Incude check for respawning

--- init.lua
+++ init.lua
@@ -6,32 +6,46 @@ minetest.register_privilege("spawn3", {description = "Third spawn.", give_to_sin
 minetest.register_privilege("spawn4", {description = "Fourth spawn.", give_to_singleplayer=false})
 
 
+-- function to select and move player to correct spawn
+local function respawn(name)
+	-- check minetest.conf file for correct coordinates depending on privs	
+	if minetest.check_player_privs(name, {spawn4=true}) then
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_4")
+	elseif minetest.check_player_privs(name, {spawn3=true}) then
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_3")
+	elseif minetest.check_player_privs(name, {spawn2=true}) then
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_2")
+	elseif minetest.check_player_privs(name, {spawn1=true}) then
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_1")
+	end
+
+	-- return if no valid spawn position...
+	if not spawnpos then
+		minetest.chat_send_player(name, "No spawn point set...")
+		return false
+	end
+
+	-- if spawn position found, teleport player
+	local player = minetest.get_player_by_name(name)
+	player:setpos(spawnpos)
+	minetest.chat_send_player(name, "Teleported to Current Spawn!")
+	return true
+end
+
+
 -- spawn command
 minetest.register_chatcommand("spawn", {
 	description = "Teleport to current spawn point.",
 	privs = {},
 	func = function(name)
-
-		-- check minetest.conf file for correct coordinates depending on privs
-		local player = minetest.get_player_by_name(name)
-		if minetest.check_player_privs(name, {spawn4=true}) then
-			spawnpos = minetest.setting_get_pos("spawn_coordinate_4")
-		elseif minetest.check_player_privs(name, {spawn3=true}) then
-			spawnpos = minetest.setting_get_pos("spawn_coordinate_3")
-		elseif minetest.check_player_privs(name, {spawn2=true}) then
-			spawnpos = minetest.setting_get_pos("spawn_coordinate_2")
-		elseif minetest.check_player_privs(name, {spawn1=true}) then
-			spawnpos = minetest.setting_get_pos("spawn_coordinate_1")
-		end
-
-		-- return if no valid spawn position...
-		if not spawnpos then
-			return false, "No spawn point set..."
-		end
-
-		-- if spawn position found, teleport player
-		player:setpos(spawnpos)
-		return true, "Teleported to Current Spawn!"
-		
+		respawn(name)
 	end
 })
+
+
+-- teleports player on respawn
+minetest.register_on_respawnplayer(function(player)
+	if respawn(player:get_player_name()) then
+		return true
+	end
+end)
